e5f6df21008aabb8a3a4d99acf33aed073d6f4b8
garciajordy <jordygarcia0119@gmail.com>
2021-10-21 13:15:05 +0200

    initial commit

--- app/models/solidus_square/gateway.rb
+++ app/models/solidus_square/gateway.rb
@@ -17,5 +17,30 @@ module SolidusSquare
     def create_customer(user, address)
       ::SolidusSquare::Customers::Create.call(client: client, spree_user: user, spree_address: address)
     end
+
+    def checkout(order, redirect_url)
+      ::SolidusSquare::Checkouts::Create.call(
+        client: client,
+        location_id: location_id,
+        order: order,
+        redirect_url: redirect_url
+      )
+      
+      ActiveMerchant::Billing::Response.new(true, 'Transaction captured', result, authorization: result.id)
+    rescue => e
+      ActiveMerchant::Billing::Response.new(false, e.message)
+    end
+
+    def capture(_amount, _response_code, gateway_options)
+      payment_method = gateway_options[:originator]
+      location_id = payment_method.preferred_location_id
+      order = gateway_options.order
+      response = checkout(idempotency_key, location_id, order, redirect_url)
+      result = response.body
+
+      ActiveMerchant::Billing::Response.new(true, 'Transaction captured', result, authorization: result.id)
+    rescue => e
+      ActiveMerchant::Billing::Response.new(false, e.message)
+    end
   end
 end
