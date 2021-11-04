fba0a24764f07966ed5240bf32f52893803120a0
garciajordy <jordygarcia0119@gmail.com>
2021-11-04 10:34:42 +0100

    Make the PaymentMethod configurable
    In this way you can choose which payment method you prefer to use
    in case you have more than 1 square payment method.

--- lib/generators/solidus_square/install/templates/initializer.rb
+++ lib/generators/solidus_square/install/templates/initializer.rb
@@ -4,6 +4,7 @@ SolidusSquare.configure do |config|
   config.square_access_token = ENV['SQUARE_ACCESS_TOKEN']
   config.square_environment = ENV['SQUARE_ENVIRONMENT']
   config.square_location_id = ENV['SQUARE_LOCATION_ID']
+  # config.square_payment_method = Spree::PaymentMethod.find_by(ENV['SQUARE_PAYMENT_METHOD'])
 end
 
 Spree::Config.configure do |config|
