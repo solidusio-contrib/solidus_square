b5d9d4f532a5092db65f93faf64c1799393a4fe0
garciajordy <jordygarcia0119@gmail.com>
2021-11-11 15:56:29 +0100

    Change the payment method in template initializer

--- lib/generators/solidus_square/install/templates/initializer.rb
+++ lib/generators/solidus_square/install/templates/initializer.rb
@@ -4,5 +4,5 @@ SolidusSquare.configure do |config|
   config.square_access_token = ENV['SQUARE_ACCESS_TOKEN']
   config.square_environment = ENV['SQUARE_ENVIRONMENT']
   config.square_location_id = ENV['SQUARE_LOCATION_ID']
-  # config.square_payment_method = Spree::PaymentMethod.find_by(ENV['SQUARE_PAYMENT_METHOD'])
+  # config.square_payment_method = Spree::PaymentMethod.find(ENV['SQUARE_PAYMENT_METHOD_ID'])
 end
