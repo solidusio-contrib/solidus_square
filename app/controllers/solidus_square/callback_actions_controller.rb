5ef20dd892d2005597d219da1df8a6162ae54772
Daniele Palombo <ksemuldie@gmail.com>
2021-11-22 18:23:14 +0100

    Add API to square_checkout
    This commit adds an API to start the Square hosted checkout

--- app/controllers/solidus_square/callback_actions_controller.rb
+++ app/controllers/solidus_square/callback_actions_controller.rb
@@ -4,15 +4,19 @@ module SolidusSquare
   class CallbackActionsController < BaseController
     include Spree::Core::ControllerHelpers::Order
 
+    before_action :load_order, only: [:square_checkout]
+
     helper_method :spree_current_user
     helper 'spree/orders'
 
     def square_checkout
+      authorize! :update, @order, order_token
+
       checkout_page_url = SolidusSquare::Gateway.new(
         access_token: SolidusSquare.config.square_access_token,
         environment: SolidusSquare.config.square_environment,
         location_id: SolidusSquare.config.square_location_id
-      ).checkout(order, redirect_url)[:checkout_page_url]
+      ).checkout(@order, redirect_url)[:checkout_page_url]
 
       respond_to do |format|
         format.html { redirect_to checkout_page_url }
@@ -21,18 +25,22 @@ module SolidusSquare
 
     def complete_checkout
       @current_order = ::Spree::Order.create(user_id: spree_current_user&.id)
-      cookies.signed[:guest_token] = current_order.guest_token
+      cookies.signed[:guest_token] = @current_order.guest_token
       redirect_to preferred_redirect_url
     end
 
     private
 
-    def order
-      @order ||= current_order
+    def order_token
+      request.headers["X-Spree-Order-Token"] || params[:order_token]
+    end
+
+    def load_order
+      @order = current_order
     end
 
     def redirect_url
-      complete_checkout_url
+      square_checkout_complete_url
     end
 
     def preferred_redirect_url
---
+++ spec/controllers/solidus_square/api/checkout_controller_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'SolidusSquare::Api::CheckoutController', type: :request do
+  let(:current_user) { create(:user) }
+  let(:order) { create(:order_with_line_items, user: current_user) }
+  let(:payment_method) { create(:square_payment_method, preferred_redirect_url: redirect_url) }
+  let(:redirect_url) { "https://github.com" }
+
+  around do |test|
+    Rails.application.routes.draw do
+      post '/api/checkouts/:id/square', to: 'solidus_square/api/checkout#create', as: 'api_checkouts_square'
+      get '/checkout/square/complete', to: 'solidus_square/callback_actions#complete_checkout',
+                                       as: :square_checkout_complete
+
+      mount Spree::Core::Engine, at: '/'
+    end
+    test.run
+    Rails.application.reload_routes!
+  end
+
+  before do
+    allow(::SolidusSquare.config).to receive(:square_payment_method).and_return(payment_method)
+  end
+
+  describe '#create', vcr: true do
+    before do
+      payment_method.preferred_redirect_url = "https://github.com"
+      payment_method.save!
+      allow(SolidusSquare.config).to receive(:square_payment_method).and_return(payment_method)
+    end
+
+    context "when format is json", vcr: true do
+      let(:json_response) { JSON.parse(response.body) }
+
+      before do
+        post api_checkouts_square_path(id: order.number, order_token: order.guest_token, format: :json)
+      end
+
+      it "returns the square URL where redirect the user" do
+        expect(response.status).to eq(200)
+        expect(json_response['checkout_page_url']).to match %r/https:\/\/connect.squareupsandbox.com\/v2\/checkout\?/
+      end
+    end
+  end
+end
