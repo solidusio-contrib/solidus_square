558e8b11de29398600c8e559fc703301e5a29b3f
Daniele Palombo <ksemuldie@gmail.com>
2021-11-24 11:31:20 +0100

    Move SolidusSquare::Gateway initializer to helper

--- app/controllers/solidus_square/api/checkout_controller.rb
+++ app/controllers/solidus_square/api/checkout_controller.rb
@@ -6,14 +6,12 @@ module SolidusSquare
       before_action :load_order, only: [:create]
       include Spree::Core::ControllerHelpers::Order
 
+      helper 'solidus_square/checkout'
+
       def create
         authorize! :update, @order, order_token
 
-        checkout_page_url = SolidusSquare::Gateway.new(
-          access_token: SolidusSquare.config.square_access_token,
-          environment: SolidusSquare.config.square_environment,
-          location_id: SolidusSquare.config.square_location_id
-        ).checkout(@order, redirect_url)[:checkout_page_url]
+        checkout_page_url = helpers.solidus_square_gateway.checkout(@order, redirect_url)[:checkout_page_url]
 
         respond_to do |format|
           format.json { render json: { checkout_page_url: checkout_page_url } }
