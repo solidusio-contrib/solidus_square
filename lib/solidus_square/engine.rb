d823659acb7d62681748882fc06cfe1a4ce4cc9e
garciajordy <jordygarcia0119@gmail.com>
2021-12-27 15:07:31 +0100

    Remove preferences from engine
    As we already provide this information in the readme file
    this should be only optional.

--- lib/solidus_square/engine.rb
+++ lib/solidus_square/engine.rb
@@ -12,17 +12,6 @@ module SolidusSquare
     engine_name 'solidus_square'
 
     initializer "solidus_square.add_static_preference", after: "spree.register.payment_methods" do |app|
-      Spree::Config.static_model_preferences.add(
-        SolidusSquare::PaymentMethod,
-        'square_credentials', {
-          access_token: SolidusSquare.config.square_access_token,
-          environment: SolidusSquare.config.square_environment,
-          location_id: SolidusSquare.config.square_location_id,
-          app_id: SolidusSquare.config.square_app_id,
-          redirect_url: ENV['SQUARE_REDIRECT_URL']
-        }
-      )
-
       app.config.spree.payment_methods << SolidusSquare::PaymentMethod
       Spree::PermittedAttributes.source_attributes.concat [:nonce]
     end
