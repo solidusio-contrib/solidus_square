097d56255a56aafd4b60fb9e93561fd0f54e1f83
Daniele Palombo <387690+DanielePalombo@users.noreply.github.com>
2022-04-11 22:27:05 +0200

    Add Spree::PaymentMethod#try_void

--- spec/models/solidus_square/payment_method_spec.rb
+++ spec/models/solidus_square/payment_method_spec.rb
@@ -10,4 +10,29 @@ RSpec.describe SolidusSquare::PaymentMethod, type: :model do
       expect(described_class.new).to be_payment_profiles_supported
     end
   end
+
+  describe '#try_void' do
+    let(:payment) { create(:payment) }
+
+    context 'when the payment cannot be voided' do
+      subject(:try_void) { described_instance.try_void(payment) }
+
+      let(:described_instance) { described_class.new }
+      let(:response) { ActiveMerchant::Billing::Response.new(true, 'Transaction voided', {}, authorization: '123') }
+
+      before do
+        allow(payment.source).to receive(:can_void?).and_return(true)
+        allow(described_instance.gateway).to receive(:void).and_return(response)
+      end
+
+      it 'calls void on the gateway' do
+        try_void
+
+        expect(described_instance.gateway).to have_received(:void).with(
+          payment.response_code,
+          originator: payment
+        )
+      end
+    end
+  end
 end
